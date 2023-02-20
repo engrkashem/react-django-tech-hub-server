4cb3f619059bebd2a96b3f981278251d20921fa8
engrkashem <kashemaust@gmail.com>
2023-02-20 20:40:56 +0600

    User model is created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    userName=models.CharField(max_length=100)
+    email=models.EmailField()
+    photo_url=models.CharField(max_length=500, blank=True)
+    skill_set=models.CharField(max_length=500, blank=True)
+    profession=models.CharField(max_length=500, blank=True)
+    user_role=models.CharField(max_length=50, blank=True)
+
+    def __str__(self) -> str:
+        return self.email
+    
+    
