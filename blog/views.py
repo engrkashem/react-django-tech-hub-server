2ef5ebd21268634b4a6bbcd8942decbe68754c35
engrkashem <kashemaust@gmail.com>
2023-02-22 18:32:47 +0600

    updating blog or patch api created and tested

--- blog/views.py
+++ blog/views.py
@@ -7,11 +7,13 @@ from rest_framework import status
 # Create your views here.
 
 class BlogView(APIView):
+    # get all blog in the database
     def get(self, request):
         blogs_obj=BlogModel.objects.all()
         serialized_blog=BlogSerializer(blogs_obj, many=True)
         return Response(serialized_blog.data, status=status.HTTP_200_OK)
     
+    # get a blog by id
     def get(self,request,pk):
         try:
             blog_obj=BlogModel.objects.get(id=pk)
@@ -21,6 +23,7 @@ class BlogView(APIView):
         serialized_blog=BlogSerializer(blog_obj)
         return Response(serialized_blog.data, status=status.HTTP_200_OK)
     
+    # create/post a blog to database
     def post(self, request):
         blog_obj=request.data
         serialized_blog=BlogSerializer(data=blog_obj)
@@ -28,3 +31,16 @@ class BlogView(APIView):
             serialized_blog.save()
             return Response(serialized_blog.data, status=status.HTTP_201_CREATED)
         return Response(serialized_blog.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self,request, pk):
+        try:
+            blog_by_id_from_db=BlogModel.objects.get(id=pk)
+        except:
+            return Response({'message':'Blog does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        updated_blog=request.data
+        serialized=BlogSerializer(blog_by_id_from_db, data=updated_blog)
+        if serialized.is_valid():
+            serialized.save()
+            return Response(serialized.data, status=status.HTTP_200_OK)
+        return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
