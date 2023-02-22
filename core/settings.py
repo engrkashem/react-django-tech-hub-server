036f9c227be5bb20f367bc6b5a6797ceabd545f6
engrkashem <kashemaust@gmail.com>
2023-02-22 11:08:36 +0600

    settings.py edited for blog, course and job app

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'users'
+    'users',
+    'blog',
+    'course',
+    'job',
 ]
 
 MIDDLEWARE = [
