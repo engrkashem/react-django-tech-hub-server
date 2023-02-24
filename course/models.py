7836304163e8c70d58189c02f1e8e16311adfc89
naimur <68554905+naimur-naiyimu@users.noreply.github.com>
2023-02-24 19:03:25 +0600

    passed instructor name

--- course/models.py
+++ course/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from users.models import User
 
 class Course(models.Model):
-    instructor = models.ForeignKey(User,  on_delete=models.CASCADE)
+    instructor = models.ForeignKey(User, related_name='instructor', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
     course_fee = models.DecimalField(decimal_places=2, max_digits=10, default=0)
--- course/serializers.py
+++ course/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import Course
-
+from users.serializers import UserSerializer
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = "__all__"
+
+    def to_representation(self, instance):
+        self.fields['instructor'] = UserSerializer(read_only=True)
+        return super().to_representation(instance)
