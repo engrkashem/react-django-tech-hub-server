bc971f99b809078c6640d9e04e5308688d6815ae
engrkashem <kashemaust@gmail.com>
2023-03-08 23:01:21 +0600

    stripe payment successfull

--- payment/views.py
+++ payment/views.py
@@ -9,16 +9,32 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 class SaveStripeInfo(APIView):
     def post(self,request):
         data = request.data
+        email = data['email']
         payment_method_id = data['payment_method_id']
-        
-        # creating customer
-        customer = stripe.Customer.create(payment_method=payment_method_id)
+        course_fee=data['course_fee']
+
+        # checking if customer already exist
+        customer_data = stripe.Customer.list(email=email).data
+
+        if len(customer_data)==0:
+            # creating customer
+            customer = stripe.Customer.create(email=email,payment_method=payment_method_id)
+        else:
+            customer = customer_data[0]
+
+        stripe.PaymentIntent.create(
+            customer=customer, 
+            payment_method=payment_method_id,  
+            currency='usd', 
+            amount=course_fee,
+            confirm=True
+        )
         
         return Response(status=status.HTTP_200_OK, 
             data={
                 'message': 'Success', 
                 'data': {'customer_id': customer.id} 
-            }  
+            } 
         ) 
 
 class StripePaymentView(APIView):
